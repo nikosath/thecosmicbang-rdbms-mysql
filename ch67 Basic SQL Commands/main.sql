7e8a24c97c953ae174eff13b705c4c3054c0ad0b
Nikos Athanasakis <emidekol@gmail.com>
2017-03-11 06:51:58 +0200

    feat: Ch67 ready to submit

--- ch67 Basic SQL Commands/main.sql
+++ ch67 Basic SQL Commands/main.sql
@@ -1,2 +1,6 @@
-create database school_db default character set utf8 collation utf8_unicode_ci;
-create table students (id int(11) not null auto_increment, first_name varchar(50) not null, last_name varchar(100) not null);
+create database school_db default character set utf8 collate utf8_unicode_ci;
+use school_db;
+create table students (id int(11) not null auto_increment, first_name varchar(50) not null, last_name varchar(100) not null, primary key(id));
+insert into students (first_name, last_name) values ('John', 'Smith');
+insert into students (first_name, last_name) values ('Mary', 'Foo');
+insert into students (first_name, last_name) values ('Paul', 'Woo');
